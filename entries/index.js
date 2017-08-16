e96ae284a224ba52885dcd6890eecab4d4c0b53c
fynnpeng <514625181@qq.com>
2017-08-16 19:54:11 +0800

    初稿

--- entries/index.js
+++ entries/index.js
@@ -2,11 +2,11 @@
  * Created by fynnpeng on 2017/8/2.
  */
 
-// import '../src/js/mobileCliImg';
+import '../src/js/mobileCliImg';
 import '../index.css';
-// document.getElementById('app').innerHTML = `
-// <div class="content">
-//     <input type="file" id="test" accept="image/*"/>
-//     <img id="showImag" width="600" height="600">
-//     <h3 id="msg">我是标题</h3>
-// </div>`;
+document.getElementById('app').innerHTML = `
+<div class="content">
+    <input type="file" id="test" accept="image/*"/>
+    <img id="showImag" width="600" height="600">
+    <h3 id="msg">我是标题</h3>
+</div>`;
